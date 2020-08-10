4b1b15acd9a0b2e6a3002c9941cd8cb7f5a639dd
Aren <reactDOM.render@yandex.ru>
2020-08-10 10:10:53 +0300

    Updates

---
+++ precache-manifest.92ee329b8c8a42f33caeee6d3fd9f65f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d09327cd49184f96419088c110d4b6",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "23acc1211d7d63ba71ce",
+    "url": "/spotify/static/css/main.fb7fe8de.chunk.css"
+  },
+  {
+    "revision": "3cd1fa231b8a60b9afb7",
+    "url": "/spotify/static/js/2.b154d237.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/spotify/static/js/2.b154d237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23acc1211d7d63ba71ce",
+    "url": "/spotify/static/js/main.3b4b9c28.chunk.js"
+  },
+  {
+    "revision": "fe96fbe7b325ae64d855",
+    "url": "/spotify/static/js/runtime-main.b428269a.js"
+  }
+]);
